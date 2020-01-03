071b9861f8a6fd8fa1113bb7bccbe7f05986be10
k8tems <k8tems@live.com>
2020-01-04 06:40:50 +0900

    Add TODO

--- test.py
+++ test.py
@@ -13,13 +13,14 @@ class TestReport(unittest.TestCase):
     def test(self):
         api = MagicMock()
         api.get.return_value = {
-            'start': 1577969903454, 'fights': [{'start_time': 496584, 'end_time': 751596}, {'start_time': 786733, 'end_time': 1059838}]}
+            'start': 1577969903454, 'fights': [{'start_time': 496584, 'end_time': 751596}]}
         report = Report.create(api, 'report_id')
 
         api.get.assert_called_with('report/fights/report_id')
         self.assertEqual(create_dt(2020, 1, 2, 21, 58, 23, 454000), report.start)
         self.assertEqual(496584, report.fights[0].ft.start_ms)
         self.assertEqual(create_dt(2020, 1, 2, 22, 6, 40, 38000), report.fights[0].ft.start_dt)
+        # TODO: end
 
 
 if __name__ == '__main__':
