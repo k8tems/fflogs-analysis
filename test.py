658f64bfc158149ffce34bf3077ddf07258d953d
k8tems <k8tems@live.com>
2020-01-04 23:19:22 +0900

    Add test for `Fight`

--- test.py
+++ test.py
@@ -24,5 +24,22 @@ class TestReport(unittest.TestCase):
         self.assertEqual(create_dt(2020, 1, 2, 22, 10, 55, 50000), report.fights[0].ft.end_dt)
 
 
+class TestFight(unittest.TestCase):
+    def test(self):
+        api = MagicMock()
+        api.get.return_value = {
+            'events': [{'timestamp': 0}]
+        }
+        start_dt = datetime(2019, 1, 1)
+        end_dt = datetime(2019, 1, 2)
+        ft = FightTime(start_dt, end_dt, 0, 0)
+        fight = Fight(api, ft, 'report_id')
+        gen = fight.gen_events('damage-done', foo='bar')
+
+        next(gen)
+        api.get.assert_called_with('report/events/damage-done/report_id', {'foo': 'bar', 'start': 0, 'end': 0})
+        print(api.get.call_args_list[0])
+
+
 if __name__ == '__main__':
     unittest.main()
